8495bdde54a25844c74fd3b9b4ce3dd0328d314f
Alessandro Koterle <alessandro.koterle@gmail.com>
2016-12-14 22:23:42 +0100

    dev: first passing test for lowering item quality at the end of the day

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,12 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void AtTheEndOfEachDayBothSellInAndQualityValuesAreLowered() {
+        Item[] items = new Item[] { new Item("foo", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
     }
 
 }
