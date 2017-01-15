b5c0d62445c28f41c946a40fb3eef1028806c607
Alessandro Koterle <alessandro.koterle@gmail.com>
2017-01-15 20:41:52 +0100

    dev: first two test written via example code

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -1,7 +1,37 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
+
+func (i Item) String() string {
+	return fmt.Sprintf("%s: sellIn %d, quality %d", i.name, i.sellIn, i.quality)
+}
 
 func Test_GildedRose(t *testing.T) {
 	main()
 }
+
+func Example_AtTheEndOfEachDayBothSellInAndQualityValuesAreLowered() {
+	items = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+	}
+	GlidedRose();
+	fmt.Println(items)
+
+	// Output:
+	// [+5 Dexterity Vest: sellIn 9, quality 19]
+}
+
+func Example__Aged_Brie__ActuallyIncreasesInQualityTheOlderItGets() {
+	items = []Item{
+		Item{"Aged Brie", 2, 0},
+	}
+	GlidedRose();
+	fmt.Println(items)
+
+	// Output:
+	// [Aged Brie: sellIn 1, quality 1]
+}
+
