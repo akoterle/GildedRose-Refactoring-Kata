63188f8179558fcddff8b80cfb89748414d07603
Alessandro Koterle <alessandro.koterle@emaze.net>
2017-02-16 09:12:18 +0100

    dev: added some tests in c

--- C/GildedRoseUnitTests.cc
+++ C/GildedRoseUnitTests.cc
@@ -22,6 +22,27 @@ TEST(TestGildedRoseGroup, FirstTest)
     STRCMP_EQUAL("fixme", items[0].name);
 }
 
+
+
+TEST(TestGildedRoseGroup, at_the_end_of_each_day_the_system_lowers_both_quality_and_sellIn_values) {
+    Item items[2];
+    init_item(items, "+5 Dexterity Vest", 10, 20);
+    update_quality(items, 1);
+    STRCMP_EQUAL("+5 Dexterity Vest", items[0].name);
+    LONGS_EQUAL(9, items[0].sellIn);
+    LONGS_EQUAL(19, items[0].quality);
+}
+
+TEST(TestGildedRoseGroup,  Once_the_sell_by_date_has_passed_Quality_degrades_twice_as_fast) {
+    Item items[2];
+    init_item(items, "+5 Dexterity Vest", 0, 10);
+    update_quality(items, 1);
+    STRCMP_EQUAL("+5 Dexterity Vest", items[0].name);
+    LONGS_EQUAL(-1, items[0].sellIn);
+    LONGS_EQUAL(8, items[0].quality);
+}
+ 
+
 void example()
 {
     Item items[6];
