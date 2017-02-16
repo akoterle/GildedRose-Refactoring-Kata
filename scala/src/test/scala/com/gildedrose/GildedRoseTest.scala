360aacf89eddf15838d133fb745744d00704aa92
Alessandro Koterle <alessandro.koterle@emaze.net>
2017-02-16 09:13:29 +0100

    dev: added a test in scala

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -3,10 +3,12 @@ package com.gildedrose
 import org.scalatest._
 
 class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
+      it should "At the end of each day our system lowers both values for every item" in {
         var items = Array[Item](new Item("foo", 0, 0))
         val app = new GildedRose(items)
         app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
+        (app.items(0).name) should equal ("foo")
+        (app.items(0).sellIn) should equal (-1)
+        (app.items(0).quality) should equal (0)
       }
 }
